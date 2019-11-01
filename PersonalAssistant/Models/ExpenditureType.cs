3045eeae7602de159c34ea5a742ed432441587e5
JackySu <jacky_su@trend.com.tw>
2019-11-01 13:01:46 +0800

    temp

---
+++ PersonalAssistant/Models/ExpenditureType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalAssistant.Models
+{
+    public class ExpenditureType
+    {
+        [Key]
+        public int ExpenditureTypeID { get; set; }
+        public string OwnerID { get; set; }
+        public string TypeName { get; set; }
+    }
+}
---
+++ PersonalAssistant/Models/StockTransaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalAssistant.Models
+{
+    public class StockTransaction
+    {
+        [Key]
+        public int StockTransactionID { get; set; }
+
+        public string OwnerID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public int AccountID { get; set; }
+
+        public AccountInitialization Account { get; set; }
+
+        [Required]
+        public string StockCode { get; set; }
+
+        [Required]
+        public StockTransactionType Type { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0, double.MaxValue)]
+        public decimal? Fees { get; set; }
+    }
+    public enum StockTransactionType
+    {
+        Buy,
+        Sell
+    }
+}
